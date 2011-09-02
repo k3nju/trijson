97f5718957b7b4bdfd59b52d2970977fa6e4aab7
n <n>
2011-09-02 07:38:49 -0700

    unicode test writing

--- test/unicode_test.cpp
+++ test/unicode_test.cpp
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
-void Test()
+uint16_t Conv2Short( char *srcVal )
 	{
-	char srcVal[] = "1234";
-	char *src = srcVal;
-	uint32_t num = 0;
-	char *p = (char*)&num;
-	for( int i = 0; i < 4; ++i, ++p )
+	uint16_t num = 0;
+	
+	for( int i = 0; i < 4; ++i )
 		{
-//		printf( "%x\n", src[i] );
+		num <<= 4;
 		// '0' - '9'
-		if( 0x30 <= *src && *src <= 0x39 )
-			*p = *src - 0x30;
+		if( 0x30 <= srcVal[i] && srcVal[i] <= 0x39 )
+			num |= srcVal[i] - 0x30;
 		// 'A' - 'F'
-		else if( 0x41 <= *src && *src <= 0x46 )
-			*p = *src - 0x37;
+		else if( 0x41 <= srcVal[i] && srcVal[i] <= 0x46 )
+			num |= srcVal[i] - 0x41 + 0x0a;
 		// 'a' - 'f'
-		else if( 0x61 <= *src && *src <= 0x66 )
-			*p = *src - 0x57;
+		else if( 0x61 <= srcVal[i] && srcVal[i] <= 0x66 )
+			num |= srcVal[i] - 0x61 + 0xa;
 		else
 			puts( "Error" );
 		}
 
-	printf( "%x\n", num );
+	return num;
+	}
+
+void Conv2ShortTest()
+	{
+	assert( Conv2Short( "0990" ) == 0x0990 );
+	assert( Conv2Short( "affa" ) == 0xaffa );
+	assert( Conv2Short( "AFFA" ) == 0xaffa );
+	assert( Conv2Short( "0aAF" ) == 0x0aaf );
+	}
+
+size_t EncodeUtf8( uint16_t src, char *dst, int dstSize )
+	{
+	
 	}
 
 int main()
 	{
-	Test();
+	Conv2ShortTest();
 	
 	return 0;
 	}
