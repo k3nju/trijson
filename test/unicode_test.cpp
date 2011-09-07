4cfca45e558226f3bb51b757967449ea1c4a3933
planet <priv.planet@gmail.com>
2011-09-08 04:52:41 +0900

    unicode conversion is difficult...

--- test/unicode_test.cpp
+++ test/unicode_test.cpp
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void Test()
+bool Str2Uint16( const char *src, uint16_t *dst )
 	{
-	char srcVal[] = "1234";
-	char *src = srcVal;
-	uint32_t num = 0;
+	uint16_t num = 0;
 	char *p = (char*)&num;
 	for( int i = 0; i < 4; ++i, ++p )
 		{
-//		printf( "%x\n", src[i] );
 		// '0' - '9'
 		if( 0x30 <= *src && *src <= 0x39 )
 			*p = *src - 0x30;
@@ -20,10 +17,34 @@ void Test()
 		else if( 0x61 <= *src && *src <= 0x66 )
 			*p = *src - 0x57;
 		else
-			puts( "Error" );
+			return false;
 		}
+	*dst = num;
+	
+	return true;
+	}
 
-	printf( "%x\n", num );
+bool Parse( const char *src, size_t srcSize, char *dst, size_t dstSize )
+	{
+	if( srcSize < 4 )
+		return false;
+
+	uint16_t num = 0;
+	if( Str2Uint16( src, &num ) == false )
+		return false;
+	srcSize -= 4;
+	
+	// surrogate pair	
+	if( 0xd800 <= num && num <= 0xdbff )
+		{
+		if( srcSize < 4 )
+			return false;
+
+		uint16_t second = 0;
+		if( Str2Uint16( 
+		}
+	
+	
 	}
 
 int main()
