5906e9b2614fb066dacff9e0eeafb120155397f8
n <n>
2011-09-08 07:10:38 -0700

    a

--- test/unicode_test.cpp
+++ test/unicode_test.cpp
@@ -1,78 +1,53 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
+#include <algorithm>
+using namespace std;
 
-<<<<<<< HEAD
-bool Str2Uint16( const char *src, uint16_t *dst )
+uint32_t StrToUint32( const char *src, size_t size )
 	{
-	uint16_t num = 0;
-	char *p = (char*)&num;
-	for( int i = 0; i < 4; ++i, ++p )
-		{
-	
-	for( int i = 0; i < 4; ++i )
+	uint32_t num = 0;
+	int count = min( (int)size, 8 );
+	for( int i = 0; i < count; ++i )
 		{
 		num <<= 4;
->>>>>>> 97f5718957b7b4bdfd59b52d2970977fa6e4aab7
-		// '0' - '9'
-		if( 0x30 <= srcVal[i] && srcVal[i] <= 0x39 )
-			num |= srcVal[i] - 0x30;
-		// 'A' - 'F'
-		else if( 0x41 <= srcVal[i] && srcVal[i] <= 0x46 )
-			num |= srcVal[i] - 0x41 + 0x0a;
-		// 'a' - 'f'
-		else if( 0x61 <= srcVal[i] && srcVal[i] <= 0x66 )
-			num |= srcVal[i] - 0x61 + 0xa;
+
+		if( 0x30 <= src[i] && src[i] <= 0x39 )
+			{
+			num |= src[i] - 0x30;
+			}
+		else if( 0x41 <= src[i] && src[i] <= 0x46 )
+			{
+			num |= src[i] - 0x37;
+			}
+		else if( 0x61 <= src[i] && src[i] <= 0x66 )
+			{
+			num |= src[i] - 0x57;
+			}
 		else
-			return false;
-		}
-	*dst = num;
-	
-	return true;
-	}
-
-bool Parse( const char *src, size_t srcSize, char *dst, size_t dstSize )
-	{
-	if( srcSize < 4 )
-		return false;
-
-	uint16_t num = 0;
-	if( Str2Uint16( src, &num ) == false )
-		return false;
-	srcSize -= 4;
-	
-	// surrogate pair	
-	if( 0xd800 <= num && num <= 0xdbff )
-		{
-		if( srcSize < 4 )
-			return false;
-
-<<<<<<< HEAD
-		uint16_t second = 0;
-		if( Str2Uint16( 
+			break;
 		}
 	
-=======
 	return num;
 	}
 
-void Conv2ShortTest()
+bool Utf16ToUtf8( 
 	{
-	assert( Conv2Short( "0990" ) == 0x0990 );
-	assert( Conv2Short( "affa" ) == 0xaffa );
-	assert( Conv2Short( "AFFA" ) == 0xaffa );
-	assert( Conv2Short( "0aAF" ) == 0x0aaf );
+	while( srcSize > 0 )
+		{
+		uint32_t codepoint = StrToUint32( src, srcSize );
+		
+		if( 0xd8000 <= codepoint && codepoint <= 0xdbff )
+			{
+			// surrogate pair
+			uint32_t codepoint2 = StrToUint32( src,
+			}
+		}
 	}
 
-size_t EncodeUtf8( uint16_t src, char *dst, int dstSize )
-	{
->>>>>>> 97f5718957b7b4bdfd59b52d2970977fa6e4aab7
-	
-	}
 
 int main()
 	{
-	Conv2ShortTest();
 	
 	return 0;
 	}
